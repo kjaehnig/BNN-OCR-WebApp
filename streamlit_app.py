0a59bbe4e9233cb3e75e2adc9cdb19d476fc3e2d
kjaehnig <jaehnig.k@gmail.com>
2024-05-01 20:09:53 -0500

    dtype chngs

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ if st.checkbox('Show Description'):
 def predict_digit_from_canvas(canvas_data, num_samples):
     if canvas_data is not None:
         # Preprocessing
-        img = grab_digits_from_canvas(canvas_data.astype('float32'))
+        img = grab_digits_from_canvas(canvas_data)
         # st.write([ii.shape for ii in img])
         # Prediction
         # pred = model.predict(img, batch_size=num_samples)  # Assume model.predict handles BNN sampling
